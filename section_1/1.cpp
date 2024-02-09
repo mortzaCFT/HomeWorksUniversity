0b7235be668995a230b106fce9d74093d1155bac
mortzaCFT <79837660+mortzaCFT@users.noreply.github.com>
2024-02-09 13:11:42 +0330

    Update 1.cpp

--- section_1/1.cpp
+++ section_1/1.cpp
@@ -1,1 +1,20 @@
 #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Method_1 using cout.
+    cout << "* * * *" << endl;
+    cout << "* * *" << endl;
+    cout << "* *" << endl;
+    cout << "*" << endl; 
+
+    //Method_2 using for loop.
+    for(int i = 0; i < 5; i++)
+    {
+        char star = '*';
+        cout << 
+    }
+  retrun 0;
+}
